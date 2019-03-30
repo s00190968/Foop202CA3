13af8afe93d8f624eb44bbba498c041ae1505d4a
Janessa <s00190968@mail.itsligo.ie>
2019-03-30 20:03:32 +0000

    Added gamesession class and a constructor for it. Next  is lesson 03.5

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,27 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModels
+{
+    class GameSession
+    {
+        Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player //player needs the player class from under models
+            {
+                Name = "Gille",
+                CharacterClass = "Bard",
+                HitPoints = 15,
+                ExperiencePoints = 0,
+                Level = 1,
+                Gold = 10
+            };
+        }
+    }
+}
