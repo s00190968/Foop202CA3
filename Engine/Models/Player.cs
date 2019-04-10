d8673954577455bb13efb1ab8955ef49307a56d7
Janessa <s00190968@mail.itsligo.ie>
2019-04-10 15:31:52 +0100

    inotify proeprty implemented in the player class

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -1,18 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Engine.Models
 {
-    public class Player
+    public class Player : INotifyPropertyChanged
     {
-        public string Name { get; set; }
-        public string CharacterClass { get; set; }
-        public int HitPoints { get; set; }
-        public int ExperiencePoints { get; set; }
-        public int Level { get; set; }
-        public int Gold { get; set; }
+        private string name;
+        private string characterClass;
+        private int hp;
+        private int xp;
+        private int level;
+        private int gold;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public string CharacterClass
+        {
+            get { return characterClass; }
+            set
+            {
+                characterClass = value;
+                OnPropertyChanged("CharacterClass");
+
+            }
+        }
+        public int HitPoints
+        {
+            get { return hp; }
+            set
+            {
+                hp = value;
+                OnPropertyChanged("HitPoints");
+
+            }
+        }
+        public int ExperiencePoints
+        {
+            get { return xp; }
+            set
+            {
+                xp = value;
+                OnPropertyChanged("ExperiencePoints");
+
+            }
+        }
+        public int Level
+        {
+            get { return level; }
+            set
+            {
+                level = value;
+                OnPropertyChanged("Level");
+
+            }
+        }
+        public int Gold
+        {
+            get { return gold; }
+            set
+            {
+                gold = value;
+                OnPropertyChanged("Gold");
+
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
